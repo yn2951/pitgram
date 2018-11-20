cf553d7ef808697b04a845be923b2062c2cb9e55
yn2951 <keichichiba@gmail.com>
2018-11-20 16:24:58 +0900

    project

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  get 'sessions/new'
+
+  root 'pages#index'
+  get 'pages/help'
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
+  resources :users
+  resources :topics
+
+  get 'favorites/index'
+  post '/favorites', to: 'favorites#create'
+end
